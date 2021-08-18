3745b1e6ede5a08b0cb73be9e54e1897f89a31f8
dmnitprof <dmn.itprof@gmail.com>
2021-08-18 16:23:58 +0300

    create first react app

---
+++ first-react-app/src/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const element = <h1>Hello World меня зовут: Дмитрий Никульшин</h1>
+
+ReactDom.render(element, document.getElementById('root'))
