a00a2eb2be0df332d9a959c35cda0d84220765a6
nikulshin.dmitriy <dmn.itprof@gmail.com>
2021-08-26 00:42:04 +0300

    add edit in project - counter

--- counter/src/components/counter.jsx
+++ counter/src/components/counter.jsx
@@ -1,43 +1,38 @@
-import React, {useState} from 'react'
+import React from 'react'
 
-const Counter = () => {
+const Counter = ({id, value, name, onIncrement, onDecrement, onDelete }) => {
 
-    const [count, setCount] = useState(0)
-
-    const formCount = () => {
-        return count === 0 ? 'Ноль' : count
+    const formValue = () => {
+        return value === 0 ? 'Ноль' : value
     }
 
     const getBadgeClasses = () => {
-        let classes = 'badge m-2 bg-'
-        classes += count === 0 ? 'danger' : 'primary'
+        let classes = 'badge p-2 m-2 bg-'
+        classes += value === 0 ? 'danger' : 'primary'
         return classes
     }
 
-    const handleIncrement = productId => {
-        console.log(productId)
-        setCount(count + 1)
-    }
-    const handleDecrement = productId => {
-        console.log(productId)
-        if(count > 0) setCount(count - 1)
-
-    }
-
     return (
-        <>
-            <span className={getBadgeClasses()}>{formCount()}</span>
+        <div>
+           <h4>{name}</h4>
+            <span className={getBadgeClasses()}>{formValue()}</span>
             <button
-                onClick={() => handleIncrement({id: 1})} className='btn btn-secondary btn-sm'
+                onClick={() => onIncrement(id, value)} className='btn btn-secondary btn-sm p-2 m-1'
             >
                 Increment
             </button>
-            <button
-                onClick={() => handleDecrement({id: 1})} className='btn btn-secondary btn-sm'
+           <button
+                onClick={() => onDecrement(id, value)} className='btn btn-secondary btn-sm p-2'
             >
                 Decrement
             </button>
-        </>
+            <button
+                className="btn btn-danger btn-sm m-2"
+                onClick={()=>onDelete(id)}
+            >
+                Delete
+            </button>
+        </div>
     )
 }
 
--- counter/src/index.js
+++ counter/src/index.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-<<<<<<< HEAD
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './components/counter'
-
-ReactDOM.render(
-    <React.StrictMode>
-        <Counter/>
-=======
 import App from './app'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -19,7 +9,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 ReactDOM.render(
     <React.StrictMode>
       <App/>
->>>>>>> 771a87a (add edit in project - counter)
     </React.StrictMode>,
     document.getElementById('root')
 )
