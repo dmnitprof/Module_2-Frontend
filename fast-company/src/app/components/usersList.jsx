3cdd1b49a205e6fddbd4927872ab5fbecf1281ed
dmnitprof <handleSearchQuery>
2021-10-16 22:49:10 +0300

    add search in userList

--- fast-company/src/app/components/usersList.jsx
+++ fast-company/src/app/components/usersList.jsx
@@ -11,6 +11,7 @@ import _ from 'lodash'
 const UsersList = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [professions, setProfession] = useState()
+    const [searchQuery, setSearchQuery] = useState('')
     const [selectedProf, setSelectedProf] = useState()
     const [sortBy, setSortBy] = useState({path: 'name', order: 'asc'})
     const pageSize = 8
@@ -40,28 +41,33 @@ const UsersList = () => {
 
     useEffect(() => {
         setCurrentPage(1)
-    }, [selectedProf])
+    }, [selectedProf, searchQuery])
 
     const handleProfessionSelect = (item) => {
+        if (searchQuery !== '') setSearchQuery('')
         setSelectedProf(item)
     }
-
+    const handleSearchQuery = ({target}) => {
+        setSelectedProf(undefined)
+        setSearchQuery(target.value)
+    }
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex)
     }
-
     const handleSort = (item) => {
         setSortBy(item)
     }
 
     if (users) {
-        const filteredUsers = selectedProf
-            ? users.filter(
-                (user) =>
-                    JSON.stringify(user.profession) ===
-                    JSON.stringify(selectedProf)
-            )
-            : users
+        const filteredUsers = searchQuery
+            ? users.filter((user) => user.name.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1)
+            : selectedProf
+                ? users.filter(
+                    (user) =>
+                        JSON.stringify(user.profession) ===
+                        JSON.stringify(selectedProf)
+                )
+                : users
         const count = filteredUsers.length
         const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order])
         const usersCrop = paginate(sortedUsers, currentPage, pageSize)
@@ -89,6 +95,13 @@ const UsersList = () => {
                 )}
                 <div className="d-flex flex-column">
                     <SearchStatus length={count}/>
+                    <input
+                        type="text"
+                        name="searchQuery"
+                        placeholder="Search..."
+                        onChange={handleSearchQuery}
+                        value={searchQuery}
+                    />
                     {count > 0 &&
                     <UserTable
                         users={usersCrop}
